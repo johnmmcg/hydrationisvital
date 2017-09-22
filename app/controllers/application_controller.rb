1e3c3ba61eeb305c42f178b3a792534f1bcf6d8c
John <john.maurer.mcguinness@gmail.com>
2017-09-22 14:08:27 -0400

    heroku config 6 editted app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :daily_goal, :avatar, :metric])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:emaill, :password, :daily_goal, :avatar, :metric])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :daily_goal, :metric])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:emaill, :password, :daily_goal, :metric])
   end
 
   def devise_mapping
