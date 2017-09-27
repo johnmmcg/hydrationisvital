52ff58d0a3a500174622fd9edac8c676850602f4
John <john.maurer.mcguinness@gmail.com>
2017-09-27 09:45:57 -0400

    heroku push

--- react/src/components/Welcome.js
+++ react/src/components/Welcome.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router';
 
 const Welcome = props => {
   return(
-    <div className="welcome animated fadeInUp">
-      <h2> hydration is vital. </h2>
-      <p> welcome text. hydration info/facts. why of the site. </p>
-      {props.children}
+    <div className="row">
+      <div className="small-12 small-centered columns welcome animated fadeInUp">
+        <h2> hydration is vital. </h2>
+        <p> You need water. Dehydration is one of the number one reasons for tiredness, headaches, and indigestion. Vital Hydration Tracker is a tool created to help people live healthier lives by being mindful of their water consumption. </p>
+        {props.children}
+      </div>
     </div>
   )
 }
